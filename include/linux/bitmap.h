0285e6d0ddeb9c3f11a407a1102277586df1582d
Yury Norov <yury.norov@gmail.com>
2021-05-06 18:02:56 -0700

    lib: extend the scope of small_const_nbits() macro
    find_bit would also benefit from small_const_nbits() optimizations.  The
    detailed comment is provided by Rasmus Villemoes.
    
    Link: https://lkml.kernel.org/r/20210401003153.97325-6-yury.norov@gmail.com
    Suggested-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    Acked-by: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Acked-by: Andy Shevchenko <andy.shevchenko@gmail.com>
    Cc: Alexey Klimov <aklimov@redhat.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: David Sterba <dsterba@suse.com>
    Cc: Dennis Zhou <dennis@kernel.org>
    Cc: Geert Uytterhoeven <geert@linux-m68k.org>
    Cc: Jianpeng Ma <jianpeng.ma@intel.com>
    Cc: Joe Perches <joe@perches.com>
    Cc: John Paul Adrian Glaubitz <glaubitz@physik.fu-berlin.de>
    Cc: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Rich Felker <dalias@libc.org>
    Cc: Stefano Brivio <sbrivio@redhat.com>
    Cc: Wei Yang <richard.weiyang@linux.alibaba.com>
    Cc: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Cc: Yoshinori Sato <ysato@users.osdn.me>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/bitmap.h
+++ include/linux/bitmap.h
@@ -229,32 +229,24 @@ int bitmap_print_to_pagebuf(bool list, char *buf,
 #define BITMAP_FIRST_WORD_MASK(start) (~0UL << ((start) & (BITS_PER_LONG - 1)))
 #define BITMAP_LAST_WORD_MASK(nbits) (~0UL >> (-(nbits) & (BITS_PER_LONG - 1)))
 
-/*
- * The static inlines below do not handle constant nbits==0 correctly,
- * so make such users (should any ever turn up) call the out-of-line
- * versions.
- */
-#define small_const_nbits(nbits) \
-	(__builtin_constant_p(nbits) && (nbits) <= BITS_PER_LONG && (nbits) > 0)
-
 #define bitmap_size(nbits)	(ALIGN(nbits, BITS_PER_LONG) / BITS_PER_BYTE)
 
 static inline void bitmap_zero(unsigned long *dst, unsigned int nbits)
 {
-	unsigned int len = bitmap_size(nbits);
+	unsigned int len = BITS_TO_LONGS(nbits) * sizeof(unsigned long);
 	memset(dst, 0, len);
 }
 
 static inline void bitmap_fill(unsigned long *dst, unsigned int nbits)
 {
-	unsigned int len = bitmap_size(nbits);
+	unsigned int len = BITS_TO_LONGS(nbits) * sizeof(unsigned long);
 	memset(dst, 0xff, len);
 }
 
 static inline void bitmap_copy(unsigned long *dst, const unsigned long *src,
 			unsigned int nbits)
 {
-	unsigned int len = bitmap_size(nbits);
+	unsigned int len = BITS_TO_LONGS(nbits) * sizeof(unsigned long);
 	memcpy(dst, src, len);
 }
 
