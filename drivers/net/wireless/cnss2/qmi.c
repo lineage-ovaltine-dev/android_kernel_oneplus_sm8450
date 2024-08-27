8faa06a08303dcea8305ecad161ffb5379abc1b1
pppig236 <priv@pppig236.com>
2024-08-27 18:48:12 -0400

    net/cnss2: Fix clang-17 errors
    Signed-off-by: pppig236 <priv@pppig236.com>

--- drivers/net/wireless/cnss2/qmi.c
+++ drivers/net/wireless/cnss2/qmi.c
@@ -645,7 +645,7 @@ static char *cnss_bdf_type_to_str(enum cnss_bdf_type bdf_type)
 
 #ifdef OPLUS_FEATURE_WIFI_BDF
 //Modify for: multi projects using different bdf
-static bool is_prj_support_region_id() {
+static bool is_prj_support_region_id(void) {
         int project_id = get_project();
         cnss_pr_dbg("project: %d\n", project_id);
         if (project_id == 20846 || project_id == 20847 || project_id == 133194
@@ -655,7 +655,7 @@ static bool is_prj_support_region_id() {
         return false;
 }
 
-static bool is_prj_need_second_rescouse() {
+static bool is_prj_need_second_rescouse(void) {
         int project_id = get_project();
         cnss_pr_dbg("project: %d\n", project_id);
         if ((project_id == 22803) || (project_id == 22881)) {
